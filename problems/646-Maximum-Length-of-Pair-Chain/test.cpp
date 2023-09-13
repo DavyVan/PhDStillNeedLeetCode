8a6165a154067ce724a62644d59bfaf902cd5020
Quan Fan <affqqa@163.com>
2023-09-13 23:33:02 +0000

    Fix: forgot to add test cases for 646

--- problems/646-Maximum-Length-of-Pair-Chain/test.cpp
+++ problems/646-Maximum-Length-of-Pair-Chain/test.cpp
@@ -1,58 +1,34 @@
 #include "kernel.h"
 #include <gtest/gtest.h>
-#include <sstream>
-#include <algorithm>
+// #include <sstream>
+// #include <algorithm>
 
 TEST(_646, _1)
 {
-    auto expect = vector<string>{"Shogun"};
-    vector<string> list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
-    vector<string> list2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
+    auto expect = 2;
+    vector<vector<int>> pairs = {
+        {1,2},
+        {2,3},
+        {3,4}
+    };
 
     Solution S;
-    auto result = S.findRestaurant(list1, list2);
+    auto result = S.findLongestChain(pairs);
 
-    sort(expect.begin(), expect.end());
-    sort(result.begin(), result.end());
-
-    EXPECT_TRUE(result.size() == expect.size());
-    for (int i = 0; i < result.size(); i++) {
-        EXPECT_TRUE(result[i] == expect[i]);
-    }
+    EXPECT_TRUE(result == expect);
 }
 
 TEST(_646, _2)
 {
-    auto expect = vector<string>{"Shogun"};
-    vector<string> list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
-    vector<string> list2 = {"KFC","Shogun","Burger King"};
+    auto expect = 3;
+    vector<vector<int>> pairs = {
+        {1,2},
+        {7,8},
+        {4,5}
+    };
 
     Solution S;
-    auto result = S.findRestaurant(list1, list2);
+    auto result = S.findLongestChain(pairs);
 
-    sort(expect.begin(), expect.end());
-    sort(result.begin(), result.end());
-
-    EXPECT_TRUE(result.size() == expect.size());
-    for (int i = 0; i < result.size(); i++) {
-        EXPECT_TRUE(result[i] == expect[i]);
-    }
-}
-
-TEST(_646, _3)
-{
-    auto expect = vector<string>{"sad","happy"};
-    vector<string> list1 = {"happy","sad","good"};
-    vector<string> list2 = {"sad","happy","good"};
-
-    Solution S;
-    auto result = S.findRestaurant(list1, list2);
-
-    sort(expect.begin(), expect.end());
-    sort(result.begin(), result.end());
-
-    EXPECT_TRUE(result.size() == expect.size());
-    for (int i = 0; i < result.size(); i++) {
-        EXPECT_TRUE(result[i] == expect[i]);
-    }
+    EXPECT_TRUE(result == expect);
 }
